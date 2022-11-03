5efde6029d4dcd7346893b72722b8c86e6cb7a87
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2022-11-03 10:45:57 +0100

    added new function in header

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@
 
 const burgerIcon = document.querySelector(".container__nav__burger-menu");
 const mobileMenu = document.querySelector(".container__nav__box");
+const closeBtn = document.querySelector(".close-btn");
 
 // Function which show our mobile menu
 const showMobileMenu = () => {
   mobileMenu.classList.toggle("show");
 };
+const closeMobileMenu = () => {
+  mobileMenu.classList.remove("show");
+};
 
 // LISTENERS
 
 burgerIcon.addEventListener("click", showMobileMenu);
+closeBtn.addEventListener("click", closeMobileMenu);
